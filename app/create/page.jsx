550e8b9fe73c001a820394f2bc8c22e02db13eca
bubblyFresh <ragnarokjaz@gmail.com>
2025-05-27 12:27:33 +0800

    fix suspense for vercel ssr

--- app/create/page.jsx
+++ app/create/page.jsx
@@ -1,48 +1,14 @@
-"use client"
-import React , {useState}  from 'react'
-import LogoTitle from './_components/LogoTitle'
-import { Button } from '@/components/ui/button'
-import { ArrowLeft , ArrowRight } from 'lucide-react'
-import LogoDesc from './_components/LogoDesc'
-import LogoColorPalette from './_components/LogoColorPalette'
-import LogoDesign from './_components/LogoDesign'
-import LogoIdea from './_components/LogoIdea'
-import GenerateLogo from './_components/GenerateLogo'
+// app/create/page.jsx
+import React, { Suspense } from 'react';
+import CreateLogo from './CreateLogo';
+import { Loader2Icon } from 'lucide-react';
 
-function CreateLogo() {
-
-    const [step , setStep] = useState(1);
-    const [formData , setFormData] = useState();
-    const onHandleInputChange = (field , value) =>{
-        setFormData((prev) => ({...prev , [field]: value}))
-    }
-    
+export default function Page() {
   return (
-    <div className='mt-28 p-8 border rounded-xl 2xl:mx-72'>
-        {step == 1?
-         <LogoTitle  onHandleInputChange={(v)=> onHandleInputChange ('title' , v)} formData={formData}/>:
-         step == 2?
-         <LogoDesc onHandleInputChange={(v)=> onHandleInputChange ('desc' , v)} formData={formData}/>:
-         step == 3?
-         <LogoColorPalette onHandleInputChange={(v)=> onHandleInputChange ('palette' , v)} formData={formData}/>:
-         step == 4?
-         <LogoDesign onHandleInputChange={(v)=> onHandleInputChange ('design' , v)} formData={formData}/>:
-         step ==5 ?
-         <LogoIdea onHandleInputChange={(v)=> onHandleInputChange ('idea' , v)} formData={formData}/>:
-          step ==6 ?
-         <GenerateLogo  formData={formData}/>:
-         null
-    }
-     
-      <div className='flex items-center justify-between mt-10'>
-        {step!=1 && <Button onClick = {() => setStep(step - 1)} variant='outline'> <ArrowLeft/>Previous</Button>}
-        {step !== 6 && <Button onClick = {() => setStep(step + 1)}> 
-          <ArrowRight /> {step !== 5 ? 'Continue' : 'Generate'}
-        </Button>}
-        
-      </div>
-    </div>
-  )
+    <main>
+      <Suspense fallback={ <div className='items-center flex justify-center'><Loader2Icon className='animate-spin text-primary w-10 h-10 mt-5'/></div>}>
+        <CreateLogo />
+      </Suspense>
+    </main>
+  );
 }
-
-export default CreateLogo
